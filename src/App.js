b925b8ee28401e90538966a94bb3cf862a242c31
Kshitij Singh <66397721+notkshitijsingh@users.noreply.github.com>
2024-04-16 12:26:58 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, {useRef} from 'react';
+
+import Logo from './assets/dmr-logo.png';
 import './App.css';
 
 function App() {
+  // product component
+  function Product({data}){
+    return(
+      <div>
+        <span>{data.name}</span>
+        <span>{data.description}</span>
+      </div>
+    );
+  }
+
+  // app render
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="navbar">
+        <div style={{flex: "1"}}>
+          <img src={Logo} alt="DMR Infosystems" style={{width: "12vw", marginLeft: "1vw"}}/>
+        </div>
+        <div className="navbar-links">
+          <span className="link-text">Home</span>
+          <span className="link-text">Products</span>
+          <span className="link-text">Services</span>
+          <span className="link-text">About Us</span>
+          <div className="button">Contact Us</div>
+        </div>
+      </div>
+      <div className="home">
+        <h3 style={{width:"100%", wordWrap:"normal"}}>
+          DMR Infosystems has been serving and been associated with government nodal agencies and service registrar for the last ten years.
+        </h3>
+      </div>
     </div>
   );
 }
