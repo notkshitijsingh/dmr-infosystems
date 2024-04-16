cbcc33ca2f5ce3adc4450d3a4fa1acc8bd7e87c3
Kshitij Singh <66397721+notkshitijsingh@users.noreply.github.com>
2024-04-17 00:28:06 +0530

    Home Stock Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useRef} from 'react';
 
+import Stock from './assets/graph.jpg';
 import Logo from './assets/dmr-logo.png';
 import './App.css';
 
@@ -16,25 +17,28 @@ function App() {
 
   // app render
   return (
-    <div className="container">
-      <div className="navbar">
-        <div style={{flex: "1"}}>
-          <img src={Logo} alt="DMR Infosystems" style={{width: "12vw", marginLeft: "1vw"}}/>
+    <>
+      <div className="container">
+        <div className="navbar">
+          <div style={{flex: "1"}}>
+            <img src={Logo} alt="DMR Infosystems" style={{width: "12vw", marginLeft: "1vw"}}/>
+          </div>
+          <div className="navbar-links">
+            <span className="link-text">Home</span>
+            <span className="link-text">Products</span>
+            <span className="link-text">Services</span>
+            <span className="link-text">About Us</span>
+            <div className="button">Contact Us</div>
+          </div>
         </div>
-        <div className="navbar-links">
-          <span className="link-text">Home</span>
-          <span className="link-text">Products</span>
-          <span className="link-text">Services</span>
-          <span className="link-text">About Us</span>
-          <div className="button">Contact Us</div>
-        </div>
+          <h3 style={{width:"100%", wordWrap:"normal", marginLeft: "25px"}}>
+            DMR Infosystems has been serving and been associated with government nodal agencies and service registrar for the last ten years.
+          </h3>
+          <img src={Stock} alt="Stock" style={{width: "100vw", height: "78vh"}}/>
       </div>
-      <div className="home">
-        <h3 style={{width:"100%", wordWrap:"normal"}}>
-          DMR Infosystems has been serving and been associated with government nodal agencies and service registrar for the last ten years.
-        </h3>
+      <div className="container">
       </div>
-    </div>
+    </>
   );
 }
 
