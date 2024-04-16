2ce2485c7ed7ab12ca7d3872c63e8fb325c18d32
Kshitij Singh <66397721+notkshitijsingh@users.noreply.github.com>
2024-04-17 00:44:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,28 +17,24 @@ function App() {
 
   // app render
   return (
-    <>
-      <div className="container">
-        <div className="navbar">
-          <div style={{flex: "1"}}>
-            <img src={Logo} alt="DMR Infosystems" style={{width: "12vw", marginLeft: "1vw"}}/>
-          </div>
-          <div className="navbar-links">
-            <span className="link-text">Home</span>
-            <span className="link-text">Products</span>
-            <span className="link-text">Services</span>
-            <span className="link-text">About Us</span>
-            <div className="button">Contact Us</div>
-          </div>
+    <div className="container">
+      <div className="navbar">
+        <div style={{flex: "1"}}>
+          <img src={Logo} alt="DMR Infosystems" style={{width: "12vw", marginLeft: "1vw"}}/>
         </div>
-          <h3 style={{width:"100%", wordWrap:"normal", marginLeft: "25px"}}>
-            DMR Infosystems has been serving and been associated with government nodal agencies and service registrar for the last ten years.
-          </h3>
-          <img src={Stock} alt="Stock" style={{width: "100vw", height: "78vh"}}/>
+        <div className="navbar-links">
+          <span className="link-text">Home</span>
+          <span className="link-text">Products</span>
+          <span className="link-text">Services</span>
+          <span className="link-text">About Us</span>
+          <div className="button">Contact Us</div>
+        </div>
       </div>
-      <div className="container">
-      </div>
-    </>
+        <h3 style={{width:"100%", wordWrap:"normal", marginLeft: "25px"}}>
+          DMR Infosystems has been serving and been associated with government nodal agencies and service registrar for the last ten years.
+        </h3>
+        <img src={Stock} alt="Stock" style={{width: "100vw", height: "78vh"}}/>
+    </div>
   );
 }
 
